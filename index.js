2dcf99b84429cdcd258d7b91833bcb652f168a90
Calypso Day <calypso.day@yahoo.com>
2019-04-04 16:13:31 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function add(x,y){
+  return x + y;
+}
+function subtract(a,b){
+  return a - b;
+}
+function multiply(a,b){
+  return a * b;
+}
+function divide(a,b){
+  return a / b;
+}
+function inc(a){
+  return ++a;
+}
+function dec
